5c8a59f9316b7925f6fae29611ac100c53b26cf9
Nate Camacho <nathan.camacho@foureyes.io>
2024-04-09 23:09:15 -0700

    Added unit tests for ticket controller and service

--- src/ticket/ticket.controller.spec.ts
+++ src/ticket/ticket.controller.spec.ts
@@ -1,20 +1,102 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TicketController } from './ticket.controller';
 import { TicketService } from './ticket.service';
+import { Ticket } from './models/ticket.model';
 
 describe('TicketController', () => {
-  let controller: TicketController;
+  let ticketController: TicketController;
+  let ticketService: TicketService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TicketController],
-      providers: [TicketService],
+      providers: [
+        {
+          provide: TicketService,
+          useValue: {
+            create: jest.fn(() => Promise.resolve()),
+            findAll: jest.fn(),
+            findOne: jest.fn(),
+            update: jest.fn(() => Promise.resolve()),
+            remove: jest.fn(() => Promise.resolve()),
+          },
+        },
+      ],
     }).compile();
 
-    controller = module.get<TicketController>(TicketController);
+    ticketController = module.get<TicketController>(TicketController);
+    ticketService = module.get<TicketService>(TicketService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('create', () => {
+    it('should create a ticket', async () => {
+      const createTicketDto = {
+        title: 'Test Ticket',
+        description: 'This is a test ticket',
+      };
+
+      await ticketController.create(createTicketDto);
+
+      expect(ticketService.create).toHaveBeenCalledWith(createTicketDto);
+    });
+  });
+
+  describe('findAll', () => {
+    it('should return all tickets', async () => {
+      const ticket1 = {
+        id: 1,
+        title: 'Test Ticket',
+        description: 'This is a test ticket',
+      } as Ticket;
+
+      const ticket2 = {
+        id: 2,
+        title: 'Test Ticket 2',
+        description: 'This is a test ticket 2',
+      } as Ticket;
+
+      const ticketList: Ticket[] = [ticket1, ticket2];
+      jest.spyOn(ticketService, 'findAll').mockResolvedValue(ticketList);
+
+      const results = await ticketController.findAll();
+
+      expect(results).toEqual(ticketList);
+    });
+  });
+
+  describe('findOne', () => {
+    it('should return single tickets', async () => {
+      const ticket1 = {
+        id: 1,
+        title: 'Test Ticket',
+        description: 'This is a test ticket',
+      } as Ticket;
+
+      jest.spyOn(ticketService, 'findOne').mockResolvedValue(ticket1);
+
+      const result = await ticketController.findOne('1');
+
+      expect(result).toEqual(ticket1);
+    });
+  });
+
+  describe('update', () => {
+    it('should update ticket', async () => {
+      const updateTicketDto = {
+        title: 'Updated Ticket',
+      };
+
+      await ticketController.update('1', updateTicketDto);
+
+      expect(ticketService.update).toHaveBeenCalledWith(1, updateTicketDto);
+    });
+  });
+
+  describe('remove', () => {
+    it('should remove ticket', async () => {
+      await ticketController.remove('1');
+
+      expect(ticketService.remove).toHaveBeenCalledWith(1);
+    });
   });
 });
--- src/ticket/ticket.service.spec.ts
+++ src/ticket/ticket.service.spec.ts
@@ -1,18 +1,121 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TicketService } from './ticket.service';
+import { getModelToken } from '@nestjs/sequelize';
+import { Ticket } from './models/ticket.model';
 
 describe('TicketService', () => {
-  let service: TicketService;
+  let ticketService: TicketService;
+
+  const mockTicketModel = {
+    create: jest.fn(() => Promise.resolve()),
+    findAll: jest.fn(),
+    findByPk: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TicketService],
+      providers: [
+        TicketService,
+        {
+          provide: getModelToken(Ticket),
+          useValue: mockTicketModel,
+        },
+      ],
     }).compile();
 
-    service = module.get<TicketService>(TicketService);
+    ticketService = module.get<TicketService>(TicketService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('create', () => {
+    it('should create a ticket', async () => {
+      const createTicketDto = {
+        title: 'Test Ticket',
+        description: 'This is a test ticket',
+      };
+
+      await ticketService.create(createTicketDto);
+
+      expect(mockTicketModel.create).toHaveBeenCalledWith(
+        expect.objectContaining({
+          title: createTicketDto.title,
+          description: createTicketDto.description,
+        }),
+      );
+    });
+  });
+
+  describe('findAll', () => {
+    it('should return all tickets', async () => {
+      const ticket1 = {
+        id: 1,
+        title: 'Test Ticket',
+        description: 'This is a test ticket',
+      } as Ticket;
+
+      const ticket2 = {
+        id: 2,
+        title: 'Test Ticket 2',
+        description: 'This is a test ticket 2',
+      } as Ticket;
+
+      const ticketList: Ticket[] = [ticket1, ticket2];
+      jest.spyOn(mockTicketModel, 'findAll').mockResolvedValue(ticketList);
+
+      const results = await ticketService.findAll();
+
+      expect(results).toEqual(ticketList);
+    });
+  });
+
+  describe('findOne', () => {
+    it('should return single tickets', async () => {
+      const ticket1 = {
+        id: 1,
+        title: 'Test Ticket',
+        description: 'This is a test ticket',
+      } as Ticket;
+
+      jest.spyOn(mockTicketModel, 'findByPk').mockResolvedValue(ticket1);
+
+      const result = await ticketService.findOne(1);
+
+      expect(result).toEqual(ticket1);
+    });
+  });
+
+  describe('update', () => {
+    it('should update ticket', async () => {
+      const updateTicketDto = {
+        title: 'Updated Ticket',
+      };
+
+      const resultTicket = {
+        id: 1,
+        update: jest.fn(),
+      };
+
+      jest.spyOn(mockTicketModel, 'findByPk').mockResolvedValue(resultTicket);
+
+      await ticketService.update(1, updateTicketDto);
+
+      expect(mockTicketModel.findByPk).toHaveBeenCalledWith(1);
+      expect(resultTicket.update).toHaveBeenCalled();
+    });
+  });
+
+  describe('remove', () => {
+    it('should remove ticket', async () => {
+      const resultTicket = {
+        id: 1,
+        destroy: jest.fn(),
+      };
+
+      jest.spyOn(mockTicketModel, 'findByPk').mockResolvedValue(resultTicket);
+
+      await ticketService.remove(1);
+
+      expect(mockTicketModel.findByPk).toHaveBeenCalledWith(1);
+      expect(resultTicket.destroy).toHaveBeenCalled();
+    });
   });
 });
